600627f84ef7bffbe6b5706ac24ed0d102b3cd20
ishaksebsib <ishaksebsib@gmail.com>
2025-06-16 00:02:41 +0300

    chore: add Docker setup for industry_context_service with env support

--- src/services/industry_context_service/src/server.ts
+++ src/services/industry_context_service/src/server.ts
@@ -1,12 +1,13 @@
 import { serve } from "@hono/node-server";
-import app from "./app.js";
+import app from "./app";
+import env from "./config/env";
 
 serve(
-	{
-		fetch: app.fetch,
-		port: 3000,
-	},
-	(info) => {
-		console.log(`Server is running on http://localhost:${info.port}`);
-	},
+  {
+    fetch: app.fetch,
+    port: env.PORT,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  },
 );
