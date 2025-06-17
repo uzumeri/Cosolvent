f7c5878efc89e85a369ee483257637f1903a498f
ishaksebsib <ishaksebsib@gmail.com>
2025-06-17 21:44:42 +0300

    feat: add tanstack query

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Roboto } from "next/font/google";
 import "./globals.css";
 import Chatbot from "@/components/chatbot/chatbot";
+import { QueryClientProvider } from "@tanstack/react-query";
+import queryClient from "@/lib/query/queryClient";
 
 const roboto = Roboto({
 	variable: "--font-roboto",
@@ -29,13 +31,15 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} ${roboto.variable} antialiased`}
-			>
-				<Chatbot />
-				{children}
-			</body>
-		</html>
+		<QueryClientProvider client={queryClient}>
+			<html lang="en">
+				<body
+					className={`${geistSans.variable} ${geistMono.variable} ${roboto.variable} antialiased`}
+				>
+					<Chatbot />
+					{children}
+				</body>
+			</html>
+		</QueryClientProvider>
 	);
 }
