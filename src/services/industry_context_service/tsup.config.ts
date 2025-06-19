932675d2d46ecf34ca5f2d62275af6faaef35400
ishaksebsib <ishaksebsib@gmail.com>
2025-06-19 21:42:19 +0300

    feat: setup woker to process uploaded documents for industry context service

--- src/services/industry_context_service/tsup.config.ts
+++ src/services/industry_context_service/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-	entry: ["src/server.ts"],
+	entry: ["src/server.ts","src/workers/index.ts"],
 	outDir: "dist",
 	format: ["esm"],
 	target: "esnext",
