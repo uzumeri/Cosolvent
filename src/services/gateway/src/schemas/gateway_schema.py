6a6441cf6f259beca579dadcc375782cc64ca52c
ishaksebsib <ishaksebsib@gmail.com>
2025-06-21 16:13:10 +0300

    chore: remove getway service

--- src/services/gateway/src/schemas/gateway_schema.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import Any
-
-class ProxyResponse(BaseModel):
-    """
-    Envelope model for proxied responses.
-    """
-    status_code: int
-    content: Any
