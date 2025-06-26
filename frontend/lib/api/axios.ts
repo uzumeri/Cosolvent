772ed274c761bd14bc9d503ab671078827094ec0
ishaksebsib <ishaksebsib@gmail.com>
2025-06-26 17:01:44 +0300

    feat: setup and config better auth client

--- frontend/lib/api/axios.ts
+++ frontend/lib/api/axios.ts
@@ -1,7 +1,8 @@
+import env from "@/config/env";
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+	baseURL: env.NEXT_PUBLIC_API_BASE_URL,
 	headers: {
 		"Content-Type": "application/json",
 	},
