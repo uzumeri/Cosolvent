d610840bd1faedbddc4f115ecff5184b093f8ff7
Tinsae Tadesse <tinsaetadesse769@gmail.com>
2025-08-14 10:59:37 +0300

    add event publishing for profile generation and approval

--- src/services/profile_management_service/src/utils/publisher.py
+++ src/services/profile_management_service/src/utils/publisher.py
@@ -7,8 +7,29 @@ from src.core.config import Settings
 logging.basicConfig(level=logging.INFO)
 
 @retry(wait=wait_fixed(2), stop=stop_after_attempt(5))
-async def publish_profile__approved(message):
+async def publish_profile_generated_event(message):
     """Publishes a message to RabbitMQ with retry handling."""
+    try:
+        connection = await aio_pika.connect_robust(Settings.Config.RABBITMQ_URL)
+        async with connection:
+            channel = await connection.channel()
+            await channel.declare_queue(QueueEventNames.profile_generation_completed, durable=True)
+            
+            await channel.default_exchange.publish(
+                aio_pika.Message(
+                    body=json.dumps(message, ensure_ascii=False).encode(),
+                    delivery_mode=aio_pika.DeliveryMode.PERSISTENT
+                ),
+                routing_key=QueueEventNames.profile_generation_completed
+            )
+        logging.info(f"Profile generated Event sent successfully for user_id:  {message.get('user_id')}")
+    except Exception as e:
+        logging.error(f"Failed to send profile generated event: {e}")
+        raise  # Re-raise to trigger retry
+
+@retry(wait=wait_fixed(2), stop=stop_after_attempt(5))
+async def publish_profile__approved(message):
+    """Publishes a profile approved message to RabbitMQ with retry handling."""
     try:
         connection = await aio_pika.connect_robust(Settings.Config.RABBITMQ_URL)
         async with connection:
@@ -22,8 +43,8 @@ async def publish_profile__approved(message):
                 ),
                 routing_key=QueueEventNames.profile_approved
             )
-        logging.info(f"Queue publish successfully for profile generation completed for user_id:  {message.get('user_id')}")
+        logging.info(f"Profile approved Event sent successfully for user_id:  {message.get('user_id')}")
     except Exception as e:
-        logging.error(f"Failed to send message: {e}")
+        logging.error(f"Failed to send profile approved event: {e}")
         raise  # Re-raise to trigger retry
 
