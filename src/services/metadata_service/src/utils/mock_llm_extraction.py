8afe235d3a45af786772c5f13b3263c2bed7dde5
Tinsae Tadesse <tinsaetadesse769@gmail.com>
2025-08-14 11:04:17 +0300

    remove merged microservices (profile_generation, user_management, translation, metadata)

--- src/services/metadata_service/src/utils/mock_llm_extraction.py
+++
@@ -1,8 +0,0 @@
-# TO-do
-
-class ExtractUsingLLM:
-    def __init__(self, url , file_type):
-        self.url = url
-        self.file_type = file_type
-    def extract(asset_id):
-        return "LLM output"
--- src/services/metadata_service/tests/conftest.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from fastapi.testclient import TestClient
-from services.metadata_service.src.main import app
-
-@pytest.fixture
-def client():
-    return TestClient(app)
